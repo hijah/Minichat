49e80bf5bd72128a02d3f7019d07fef973834a56
jacob hansen <jacob.aa@hotmail.com>
2017-09-05 11:15:42 +0200

    Lavet opgave 1 (server og start metode)

---
+++ MiniChatServer/MiniChatServer/Server.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MiniChatServer
+{
+    public class Server
+    {
+        public void start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Loopback, 7070);
+            server.Start();
+
+            using (TcpClient client = server.AcceptTcpClient())
+            using (NetworkStream ns = client.GetStream())
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+                while (true)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                    string myline = Console.ReadLine();
+                    sw.WriteLine(myline);
+
+                    sw.Flush();
+                    if (myline == "STOP" || line == "STOP")
+                    {
+                        break;
+                    }
+                }
+            server.Stop();
+
+
+        }
+    }
+}
