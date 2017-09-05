9f985c864669c70bb6c8d4e7ca3d8c3e029f887a
jacob hansen <jacob.aa@hotmail.com>
2017-09-05 11:32:13 +0200

    Tilføjet client.

--- MiniChatServer/MiniChatClient/Client.cs
+++ MiniChatServer/MiniChatClient/Client.cs
@@ -1,38 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace MiniChatServer
+namespace MiniChatClient
 {
-    public class Server
+    class Client
     {
-
         public void start()
         {
-            TcpListener server = new TcpListener(IPAddress.Loopback, 7070);
-            server.Start();
-
-            using (TcpClient client = server.AcceptTcpClient())
-            using (NetworkStream ns = client.GetStream())
+            using (TcpClient socket = new TcpClient("localhost", 7070))
+            using (NetworkStream ns = socket.GetStream())
             using (StreamReader sr = new StreamReader(ns))
             using (StreamWriter sw = new StreamWriter(ns))
                 while (true)
                 {
-                    string line = sr.ReadLine();
-                    Console.WriteLine(line);
                     string myline = Console.ReadLine();
                     sw.WriteLine(myline);
-
                     sw.Flush();
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
                     if (myline == "STOP" || line == "STOP")
                     {
                         break;
                     }
                 }
-            server.Stop();
-
-
         }
     }
 }
--- MiniChatServer/MiniChatClient/Program.cs
+++ MiniChatServer/MiniChatClient/Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MiniChatServer
+namespace MiniChatClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Server server = new Server();
-            server.start();
+            Client client = new Client();
+            client.start();
 
             
         }
